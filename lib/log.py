8d135ded5501f311c5a8b023acd99cc4f257f2db
askme-abhi <abhishek@askme.in>
2016-03-14 11:56:03 +0530

    changed file handler to TimeRotatingFileHandler

--- lib/log.py
+++ lib/log.py
@@ -4,14 +4,17 @@ from config import LOG_FILE, LOG_FILE_ERROR
 
 
 def setup_logging():
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    handler = logging.handlers.WatchedFileHandler(LOG_FILE)
-    handler.setLevel(logging.DEBUG)
-    errorhandler = logging.handlers.WatchedFileHandler(LOG_FILE_ERROR)
-    errorhandler.setLevel(logging.ERROR)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(funcName)s-[in %(pathname)s:%(lineno)d]- %(message)s")
+    formatter = logging.Formatter("[ %(asctime)s - %(levelname)s - %(pathname)s - %(module)s - %(funcName)s - %(lineno)d ] - %(message)s")
+    logger = logging.getLogger('couponservice')
+    logger.setLevel(logging.INFO)
+
+    handler = logging.handlers.TimedRotatingFileHandler(LOG_FILE)
+    handler.setLevel(logging.INFO)
     handler.setFormatter(formatter)
+
+    errorhandler = logging.handlers.TimedRotatingFileHandler(LOG_FILE_ERROR)
+    errorhandler.setLevel(logging.ERROR)
     errorhandler.setFormatter(formatter)
+
     logger.addHandler(handler)
     logger.addHandler(errorhandler)
