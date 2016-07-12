93cd854e7e86abf365d185bd4b2f7ef953b65643
askme-abhi <abhishek@askme.in>
2016-07-12 13:23:03 +0530

    misc updates to the script for ibl vouchers

--- src/scripts.py
+++ src/scripts.py
@@ -7,7 +7,11 @@ import importlib
 from src.rules.rule import Rule, Benefits
 from config import method_dict
 import hashlib
-
+import gevent
+import gevent.monkey
+import datetime
+import json
+gevent.monkey.patch_all()
 
 def do_it_now(body, params=dict()):
     headers = {
@@ -26,26 +30,111 @@ def create_it_now(body, params=dict()):
     r = make_api_call('http://api-service04.production.askme.com:9933/vouchers/v1/create', method='POST', headers=headers, body=body, params=params)
     return r
 
+def fetch_ibl(body, params=dict()):
+    headers = {
+        'X-API-USER': 'admin',
+        'X-API-TOKEN': 'kuchbhi'
+    }
+    r = make_api_call('http://api-service04.production.askme.com:9933/vouchers/v1/fetchDetail', method='POST', headers=headers, body=body, params=params)
+#    r = make_api_call('http://pyservice01.staging.askme.com:9933/vouchers/v1/fetchDetail', method='POST', headers=headers, body=body, params=params)
+    return r
+
+def create_ibl(body, params=dict()):
+    headers = {
+        'X-API-USER': 'admin',
+        'X-API-TOKEN': 'kuchbhi'
+    }
+#    r = make_api_call('http://api-service04.production.askme.com:9933/vouchers/v1/update', method='POST', headers=headers, body=body, params=params)
+    r = make_api_call('http://api-service04.production.askme.com:9933/vouchers/v1/create', method='POST', headers=headers, body=body, params=params)
+    return r
+
+def process_one_list(a_list):
+        if '2016006271REP' in a_list:
+            a_list.remove('2016006271REP')
+        codes = [coupon['code'] for coupon in a_list]
+        body = {
+                "coupon_codes": codes
+                }
+        r = fetch_ibl(body=body)
+        with open('/Users/abhi/Documents/output_ibl_fetch.log', 'a+') as f:
+            f.write(r.text)
+            f.close()
+        if r.status_code != 200:
+            return None
+        resp_data = r.json()
+        coupon_list = resp_data['data']['success_list']
+        a_map = dict()
+        for a_data in a_list:
+            a_map[a_data['code']] = a_data
+        coupon_map = dict()
+#        print codes.__len__()
+#        print coupon_list.__len__()
+        for coupon in coupon_list:
+#            print("\n************************\n")
+#            print coupon
+#            print("\n\n\n\n\n************************\n")
+            found_coupon = coupon_map.get(coupon['code'])
+            if not found_coupon or found_coupon['from'] < coupon['from']:
+                coupon_map[coupon['code']] = coupon
+#        print coupon_map.__len__()
+#        print("\n\n\n\n\n************************\n***********************\n\n\n\n")
+        for code, coupon in coupon_map.iteritems():
+            body = coupon
+            body['from'] = '2016-06-15'
+            body['to'] = a_map[coupon['code']]['to']
+            del body['rules'][0]['benefits']
+            r = create_ibl(body=body)
+            with open('/Users/abhi/Documents/output_ibl_create.log', 'a+') as f:
+                f.write(r.text)
+                f.close()
+
+
+def recreate_ibl():
+    to = '2016-06-14'
+    db = CouponsAlchemyDB()
+    get_voucher_query = "select code, `from`, `to` from all_vouchers where `to`> :to  and ( code rlike '20[1-5][0-9][0-9]{6}NEW'  or code rlike '20[1-5][0-9][0-9]{6}TWO' or code rlike '20[1-5][0-9][0-9]{6}REP')"
+    code_from_list = db.execute_raw_sql(get_voucher_query, {'to': to})
+    code_from_list = [{'to': code_from_dict['to'].isoformat(), 'from': code_from_dict['from'].isoformat(), 'code': code_from_dict['code']} for code_from_dict in code_from_list]
+    print code_from_list.__len__()
+    lists = list(chunks(code_from_list, 200))
+#    process_one_list(lists[0])
+    threads = [gevent.spawn(process_one_list, a_list) for a_list in lists]
+    gevent.joinall(threads)
+
+
+#    for a_list in lists:
+#        gevent.spawn(process_one_list, a_list)
+
+#        with open('/Users/abhi/Documents/output_ibl_expire.log', 'a+') as f:
+#            f.write(r.text)
+#            f.close()
+
+
 
 def do_iocl_date_correction():
-    date_value = "2016-08-30 18:30:00.000000"
-    date_value = parser.parse(date_value)
+    to = '2016-06-14'
     db = CouponsAlchemyDB()
-    get_voucher_query = 'select `code`, `from` from all_vouchers where `to` = :to'
-    code_from_list = db.execute_raw_sql(get_voucher_query, {'to': date_value})
-    code_from_list = [{'from': code_from_dict['from'].isoformat(), 'code': code_from_dict['code']} for code_from_dict in code_from_list]
-    lists = list(chunks(code_from_list, 5000))
+    get_voucher_query = "select code, `from`, `to` from all_vouchers where `to`> :to  and ( code rlike '20[1-5][0-9][0-9]{6}NEW'  or code rlike '20[1-5][0-9][0-9]{6}TWO' or code rlike '20[1-5][0-9][0-9]{6}REP')"
+    code_from_list = db.execute_raw_sql(get_voucher_query, {'to': to})
+    code_from_list = [{'to': code_from_dict['to'].isoformat(), 'from': code_from_dict['from'].isoformat(), 'code': code_from_dict['code']} for code_from_dict in code_from_list]
+    lists = list(chunks(code_from_list, 2000))
     for a_list in lists:
-        body = [
-            {
-                "coupons": a_list,
+        body = list()
+        for coupon in a_list:
+            if coupon['code'] == '2016006271REP':
+                continue
+            body_dict = {
+                "coupons": [{
+                    'code': coupon['code'],
+                    'from': coupon['from']
+                }],
                 "update": {
-                    "to": date_value.isoformat()
+                    "to": '2016-01-01'
                 }
             }
-        ]
+            body.append(body_dict)
         r = do_it_now(body=body)
-        with open('/Users/harshmathur/Documents/update_to_date/output_iocl.log', 'a+') as f:
+        with open('/Users/abhi/Documents/output_ibl_expire.log', 'a+') as f:
             f.write(r.text)
             f.close()
 
